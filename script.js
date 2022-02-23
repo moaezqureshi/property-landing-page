8f7432b411f90b7b9ed7f4181653885f24aaa2db
Moaez Qureshi <moaez135@gmail.com>
2022-02-23 13:39:43 +0530

    first and final commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function show() {
+    let btn = document.getElementById('mobile-menu-button');
+    let menu = document.getElementById('mobile-menu');
+    
+    menu.classList.toggle("hidden");
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['ui-monospace', 'SFMono-Regular'],
+      'blinker': ['"blinker"','"sans-serif"'],
+      'Fira-sans': ['"Fira sans"','"sans-serif"'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
